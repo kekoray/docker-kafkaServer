55f087d9731b46b83e371ad129ebd697bda980b8
kekoray <61939347+kekoray@users.noreply.github.com>
2023-09-08 10:40:47 +0800

    1

--- kafka_2.2_arm64/start.sh
+++ kafka_2.2_arm64/start.sh
@@ -11,6 +11,7 @@ fi
 
 # docker-compose传参
 echo "-----------  kafka is starting  ------------"
+# (本机IP/FRP云服务IP)
 IP=$(ip addr show eth0 | grep -Po 'inet \K[\d.]+')
 externalIP="$IP" \
 managerUser="root" \
--- kafka_2.2_arm64/stop.sh
+++ kafka_2.2_arm64/stop.sh
@@ -1,4 +1,5 @@
 echo "-----------  kafka is stopping  ------------"
+# (本机IP/FRP云服务IP)
 IP=$(ip addr show eth0 | grep -Po 'inet \K[\d.]+')
 externalIP="$IP" \
 managerUser="root" \
